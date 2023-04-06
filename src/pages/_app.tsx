bd020a42153b9499b81c80b1d3499982745d18d1
sjohnston82 <72237516+sjohnston82@users.noreply.github.com>
2023-04-06 08:58:49 -0400

    moving head component to app level

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { SessionProvider } from "next-auth/react";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,6 +13,14 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>QuarterMaster</title>
+        <meta
+          name="description"
+          content="QuarterMaster pantry inventory manager"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
