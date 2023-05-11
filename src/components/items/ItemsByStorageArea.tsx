1f719d3c9614692e66541786f95186f04be3486d
sjohnston82 <72237516+sjohnston82@users.noreply.github.com>
2023-05-11 06:17:33 -0400

    updated loading logic for sort by storage area

--- src/components/items/ItemsByStorageArea.tsx
+++ src/components/items/ItemsByStorageArea.tsx
@@ -2,30 +2,54 @@ import React from "react";
 import Banner from "../ui/Banner";
 import { api } from "~/utils/api";
 import Item from "./Item";
+import LoadingSpinner from "../ui/LoadingSpinner";
 
 interface ItemsByStorageAreaProps {
-
   storageAreaId: string;
 }
 
 const ItemsByStorageArea = ({ storageAreaId }: ItemsByStorageAreaProps) => {
+  const shouldEnableQuery =
+    !!storageAreaId && storageAreaId !== undefined && storageAreaId !== null;
 
-  const getCurrentStorageArea = api.storageAreas.getStorageAreaById.useQuery({
-    storageAreaId,
-  });
-  const { data, isLoading } = api.items.getItemsByStorageArea.useQuery({
-    storageAreaId,
-  });
+  const getCurrentStorageArea = api.storageAreas.getStorageAreaById.useQuery(
+    {
+      storageAreaId,
+    },
+    {
+      enabled: !!storageAreaId,
+    }
+  );
+  const { data, isLoading } = api.items.getItemsByStorageArea.useQuery(
+    {
+      storageAreaId,
+    },
+    {
+      enabled: !!storageAreaId,
+    }
+  );
+
+  const shouldShowLoading = shouldEnableQuery && isLoading;
   return (
     <div>
       <Banner>
         {getCurrentStorageArea.data && getCurrentStorageArea.data.name}
       </Banner>
-      {data?.length === 0 ? <p className="mx-2 text-center mt-5">There are no items currently saved in this storage area.</p> : 
-      data?.map((item) => (
-      
-        <Item key={item.id} {...item} />
-      ))}
+      {shouldShowLoading && (
+        <div className="relative mt-20 flex h-full flex-col items-center justify-center gap-2">
+          <div className="absolute  top-1/2 flex h-full w-full flex-col items-center justify-center ">
+            <p className="font-semibold ">Fetching items...</p>
+            <LoadingSpinner size={60} />
+          </div>
+        </div>
+      )}
+      {data?.length === 0 ? (
+        <p className="mx-2 mt-5 text-center">
+          There are no items currently saved in this storage area.
+        </p>
+      ) : (
+        data?.map((item) => <Item key={item.id} {...item} />)
+      )}
     </div>
   );
 };
