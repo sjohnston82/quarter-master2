1449e5484912478123dbe15625e6a1e34be50704
sjohnston82 <72237516+sjohnston82@users.noreply.github.com>
2023-06-12 10:43:15 -0400

    fixed food type icons

--- src/utils/helperLists.ts
+++ src/utils/helperLists.ts
@@ -37,54 +37,41 @@ export const groceryStoreAreas = [
   "Other",
 ];
 
-export const chipIcons = [
-  {
-    produce:
-      "https://freeiconshop.com/wp-content/uploads/edd/apple-outline-filled.png",
-  },
-  {
-    canned:
-      "https://www.clipartmax.com/png/middle/29-293605_pin-canned-food-clipart-canned-food-icon-png.png",
-  },
-  {
-    pasta:
-      "https://icons.veryicon.com/64/Food%20%26%20Drinks/3D%20Food/Dish%20Pasta%20Spaghetti.png",
-  },
-  {
-    spices: "https://icon-library.com/images/spice-icon/spice-icon-12.jpg",
-  },
-  {
-    meats:
-      "https://freeiconshop.com/wp-content/uploads/edd/meat-outline-filled.png",
-  },
-  {
-    dairy:
-      "https://icons.veryicon.com/png/o/food--drinks/linear-icons-commonly-used-in-convenience/dairy-4.png",
-  },
-  {
-    snacks:
-      "https://icons.veryicon.com/png/o/food--drinks/new-nut-series-icon-in-early-spring/giant-snack-bag.png",
-  },
-  {
-    frozen:
-      "https://icons.veryicon.com/png/o/miscellaneous/utility/frozen-10.png",
-  },
-  {
-    breads:
-      "https://icons.veryicon.com/png/o/food--drinks/new-food-in-spring/bread-10.png",
-  },
-  {
-    ingredients:
-      "https://icons.veryicon.com/png/o/food--drinks/delicious-food-3/vinegar-vinegar.png",
-  },
-  {
-    seafood:
-      "https://www.veryicon.com/icons/food--drinks/new-food-in-spring/seafood-2.html",
-  },
-  {
-    desserts:
-      "https://icons.veryicon.com/png/o/cartoon/christmas-icon/dessert-2.png",
-  },
-];
+export const chipIcons = {
+  produce:
+    "https://freeiconshop.com/wp-content/uploads/edd/apple-outline-filled.png",
+
+  canned:
+    "https://www.clipartmax.com/png/middle/29-293605_pin-canned-food-clipart-canned-food-icon-png.png",
+
+  pasta:
+    "https://icons.veryicon.com/64/Food%20%26%20Drinks/3D%20Food/Dish%20Pasta%20Spaghetti.png",
+
+  spices: "https://icon-library.com/images/spice-icon/spice-icon-12.jpg",
+
+  meats:
+    "https://freeiconshop.com/wp-content/uploads/edd/meat-outline-filled.png",
+
+  dairy:
+    "https://icons.veryicon.com/png/o/food--drinks/linear-icons-commonly-used-in-convenience/dairy-4.png",
+
+  snacks:
+    "https://icons.veryicon.com/png/o/food--drinks/new-nut-series-icon-in-early-spring/giant-snack-bag.png",
+
+  frozen:
+    "https://icons.veryicon.com/png/o/miscellaneous/utility/frozen-10.png",
+
+  breads:
+    "https://icons.veryicon.com/png/o/food--drinks/new-food-in-spring/bread-10.png",
+
+  ingredients:
+    "https://icons.veryicon.com/png/o/food--drinks/delicious-food-3/vinegar-vinegar.png",
+
+  seafood:
+    "https://www.veryicon.com/icons/food--drinks/new-food-in-spring/seafood-2.html",
+
+  desserts:
+    "https://icons.veryicon.com/png/o/cartoon/christmas-icon/dessert-2.png",
+};
 
 export const shoppingListAmountTypes = ["lbs", "oz", "g", "kgs"];
